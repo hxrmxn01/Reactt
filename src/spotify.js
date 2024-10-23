3870664cae75a4fbc465e6462e00338472a6319b
Harmanjot Singh <hsbd0761@gmail.com>
2024-10-24 03:44:58 +1400

    clid

--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?";
-const clientId = "5b0805ee6f944b2d910da9eeabb32f9f";
+const clientId = "717dc35471f84681a0b6f1192f1c85da";
 const redirectUri = "https://vibinn.vercel.app/";
 const scopes = ["user-library-read", "playlist-read-private"];
 
